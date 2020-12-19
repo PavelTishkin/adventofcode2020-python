0b781d2bdd0f672822c31480b2c58e650b722740
Pavel Tishkin <18540358+PavelTishkin@users.noreply.github.com>
2020-12-19 14:45:14 +0000

    Day 18 part 2 solution

--- day18/main.py
+++ day18/main.py
@@ -5,43 +5,83 @@ def main():
 
     eq_sum = 0
     for equation in equations:
-        eq_sum += solve_equation(equation)
+        eq = Equation(equation)
+        eq_sum += eq.answer
     print('Answer 1: {}'.format(eq_sum))
 
-
-def solve_equation(equation):
-    result, _ = solve_nested_equation(equation)
-    return result
-
-
-def solve_nested_equation(equation):
-    lhs = 0
-    op = '+'
-    curr_offset = 0
-    i = 0
-    while i < len(equation):
-        curr_offset += 1
-        symbol = equation[i]
-        i += 1
-        if symbol in '0123456789':
-            lhs = calculate(lhs, int(symbol), op)
-        elif symbol in ['+', '*']:
-            op = symbol
-        elif symbol == '(':
-            rhs, offset = solve_nested_equation(equation[i:])
-            i += offset
-            curr_offset += offset
-            lhs = calculate(lhs, rhs, op)
-        elif symbol == ')':
-            return lhs, curr_offset
-    return lhs, 0
+    eq_sum = 0
+    for equation in equations:
+        eq = Equation(equation, True)
+        eq_sum += eq.answer
+    print('Answer 2: {}'.format(eq_sum))
 
 
-def calculate(lhs, rhs, op):
-    if op == '+':
-        return lhs + rhs
-    elif op == '*':
-        return lhs * rhs
+class Equation:
+
+    def __init__(self, equation_text, is_addition_preference=False):
+        self.equation_text = equation_text
+        self.is_addition_preference = is_addition_preference
+        self.elements = []
+        self.answer = 0
+        self.extract_elements()
+        self.solve()
+
+    def extract_elements(self):
+        ptr = 0
+        while ptr < len(self.equation_text):
+            symbol = self.equation_text[ptr]
+            if symbol in '0123456789':
+                self.elements.append(int(symbol))
+            elif symbol in ['+', '*']:
+                self.elements.append(symbol)
+            elif symbol == '(':
+                sub_equation = Equation(self.equation_text[ptr+1:], self.is_addition_preference)
+                ptr += len(sub_equation.equation_text) + 1
+                self.elements.append(sub_equation.answer)
+                None
+            elif symbol == ')':
+                self.equation_text = self.equation_text[0:ptr]
+                break
+            ptr += 1
+
+    def solve(self):
+        if self.is_addition_preference:
+            reduced_elements = self.reduce_for_ops(['+'])
+            reduced_elements = self.reduce_for_ops(['*'], reduced_elements)
+        else:
+            reduced_elements = self.reduce_for_ops(['+', '*'])
+
+        self.answer = reduced_elements[0]
+
+    def reduce_for_ops(self, ops, elements=[]):
+        reduced_elements = elements[:]
+        if not elements:
+            reduced_elements = self.elements[:]
+        while any(op in reduced_elements for op in ops):
+            for ptr, symbol in enumerate(reduced_elements):
+                if symbol in ops:
+                    lhs = reduced_elements[ptr - 1]
+                    rhs = reduced_elements[ptr + 1]
+                    lhs = self.calculate(lhs, rhs, symbol)
+                    new_reduced_elements = []
+                    if ptr > 2:
+                        new_reduced_elements = reduced_elements[:ptr-1]
+                    new_reduced_elements.append(lhs)
+                    new_reduced_elements.extend(reduced_elements[ptr+2:])
+                    reduced_elements = new_reduced_elements
+                    break
+
+        return reduced_elements
+
+    @staticmethod
+    def calculate(lhs, rhs, op):
+        if op == '+':
+            return lhs + rhs
+        elif op == '*':
+            return lhs * rhs
+
+    def __repr__(self):
+        return self.equation_text
 
 
 if __name__ == '__main__':
--- day18/main_test.py
+++ day18/main_test.py
@@ -5,18 +5,59 @@ from day18 import main
 
 class MainTestCase(unittest.TestCase):
 
-    def test_solve_simple_equation_returns_correct_values(self):
-        eq = '1 + 2 * 3 + 4 * 5 + 6'
-        actual = main.solve_equation(eq)
-        self.assertEqual(actual, 71)
+    def test_solve_simple_equation_extracts_correct_elements(self):
+        eq = main.Equation('1 + 2 * 3')
+        self.assertEqual(eq.elements, [1, '+', 2, '*', 3])
 
-    def test_solve_nested_equation_returns_correct_values(self):
-        eq = '1 + (2 * 3) + (4 * (5 + 6))'
-        actual = main.solve_equation(eq)
-        self.assertEqual(actual, 51)
+    def test_reduce_for_ops_returns_correct_elements(self):
+        eq = main.Equation('1 + 2 * 3')
+        elements = eq.reduce_for_ops(['+'])
+        self.assertEqual(elements, [3, '*', 3])
 
-    def test_solve_equation_starting_with_bracket_returns_correct_values(self):
-        eq = '((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2'
-        actual = main.solve_equation(eq)
-        self.assertEqual(actual, 13632)
+    def test_solve_equation_returns_correct_values(self):
+        eq = main.Equation('1 + 2 * 3 + 4 * 5 + 6')
+        self.assertEqual(eq.answer, 71)
 
+    def test_solve_equation_returns_correct_values2(self):
+        eq = main.Equation('1 + (2 * 3) + (4 * (5 + 6))')
+        self.assertEqual(eq.answer, 51)
+
+    def test_solve_equation_returns_correct_values3(self):
+        eq = main.Equation('2 * 3 + (4 * 5)')
+        self.assertEqual(eq.answer, 26)
+
+    def test_solve_equation_returns_correct_values4(self):
+        eq = main.Equation('5 + (8 * 3 + 9 + 3 * 4 * 3)')
+        self.assertEqual(eq.answer, 437)
+
+    def test_solve_equation_returns_correct_values5(self):
+        eq = main.Equation('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))')
+        self.assertEqual(eq.answer, 12240)
+
+    def test_solve_equation_returns_correct_values6(self):
+        eq = main.Equation('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2')
+        self.assertEqual(eq.answer, 13632)
+
+    def test_solve_equation_addition_preference_returns_correct_values1(self):
+        eq = main.Equation('1 + 2 * 3 + 4 * 5 + 6', True)
+        self.assertEqual(eq.answer, 231)
+
+    def test_solve_equation_addition_preference_returns_correct_values2(self):
+        eq = main.Equation('1 + (2 * 3) + (4 * (5 + 6))', True)
+        self.assertEqual(eq.answer, 51)
+
+    def test_solve_equation_addition_preference_returns_correct_values3(self):
+        eq = main.Equation('2 * 3 + (4 * 5)', True)
+        self.assertEqual(eq.answer, 46)
+
+    def test_solve_equation_addition_preference_returns_correct_values4(self):
+        eq = main.Equation('5 + (8 * 3 + 9 + 3 * 4 * 3', True)
+        self.assertEqual(eq.answer, 1445)
+
+    def test_solve_equation_addition_preference_returns_correct_values5(self):
+        eq = main.Equation('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))', True)
+        self.assertEqual(eq.answer, 669060)
+
+    def test_solve_equation_addition_preference_returns_correct_values6(self):
+        eq = main.Equation('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2', True)
+        self.assertEqual(eq.answer, 23340)
