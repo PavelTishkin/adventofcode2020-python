7675f9ae1f6959a3aee3e89f92fe3addd24e9d76
Pavel Tishkin <18540358+PavelTishkin@users.noreply.github.com>
2020-12-24 01:56:01 +0000

    Day 20 part 2 solution

--- day20/main.py
+++ day20/main.py
@@ -1,5 +1,18 @@
 import re
 
+rotation_matrix = [
+    [0, 3, 2, 1],
+    [1, 0, 3, 2],
+    [2, 1, 0, 3],
+    [3, 2, 1, 0]
+]
+
+sea_monster = [
+    '                  # ',
+    '#    ##    ##    ###',
+    ' #  #  #  #  #  #   '
+]
+
 
 def main():
     input_file = open('input/day20.txt', 'r')
@@ -7,16 +20,33 @@ def main():
     input_file.close()
 
     corners_product = 1
+    corner_tile_id = 0
     tiles = parse_tiles(tiles_txt)
     for tile in tiles.values():
         tile.set_adjacent_tiles(tiles.values())
         if len(tile.adjacent_tiles) == 2:
+            corner_tile_id = tile.tile_id
             corners_product *= tile.tile_id
 
     print('Answer 1: {}'.format(corners_product))
 
+    normalize_tiles(corner_tile_id, tiles)
+    tile_array = compile_tile_array(corner_tile_id, tiles)
+
+    image = get_whole_picture(tile_array)
+    nessie_count = get_nessie_count(image, sea_monster)
+    pixels_count = count_chars(image, '#')
+    nessie_pixels_count = count_chars(sea_monster, '#')
+
+    print('Answer 2: {}'.format(pixels_count - nessie_pixels_count * nessie_count))
+
 
 def parse_tiles(tiles_txt):
+    """
+    Parse text data containing list of tiles
+    :param tiles_txt: Text data
+    :return: Set of tiles using tile id as a key
+    """
     tiles = {}
 
     tile_id = 0
@@ -43,6 +73,114 @@ def set_adjacent_tiles(tiles):
         tile.set_adjacent_tiles(tiles.values())
 
 
+def transform_2d_array(original, rotate=0, flip=False):
+    """
+    Transforms a 2d array through rotation and mirror flip
+    :param original: Original array to transform
+    :param rotate: Number of times to rotate original array clockwise 90 degrees
+    :param flip: True to mirror flip array
+    :return: Transformed array
+    """
+    transformed = original
+    for i in range(rotate):
+        transformed = [list(r) for r in zip(*transformed[::-1])]
+    if flip:
+        transformed = transformed[:]
+        transformed.reverse()
+
+    return transformed
+
+
+def normalize_tiles(corner_tile_id, all_tiles):
+    """
+    Rotates and flips tiles, using a selected corner tile as a starting point until all tiles are oriented 
+    and flipped in the same direction
+    """
+    corner_tile = all_tiles[corner_tile_id]
+    corner_tile.set_neighbors_flipped('normal')
+    for tile in all_tiles.values():
+        if tile.is_flipped == 'flipped':
+            tile.is_flipped = 'normal'
+            flipped_data = transform_2d_array(tile.tile_cells, 0, True)
+            tile.tile_cells = flipped_data
+            tile.load_edges()
+    set_adjacent_tiles(all_tiles)
+    corner_tile = all_tiles[corner_tile_id]
+    rotated_data = transform_2d_array(corner_tile.tile_cells,
+                                      (1 - min(corner_tile.adjacent_tiles.keys())) % 4, False)
+    all_tiles[corner_tile_id] = Tile(corner_tile_id, rotated_data)
+    corner_tile = all_tiles[corner_tile_id]
+    corner_tile.is_rotated = True
+    set_adjacent_tiles(all_tiles)
+    corner_tile.rotate_neighbours(all_tiles)
+
+
+def compile_tile_array(corner_tile_id, all_tiles):
+    tiles_array = []
+
+    next_edge_tile = all_tiles[corner_tile_id]
+    while True:
+        next_tile = next_edge_tile
+        tiles_row = [next_tile]
+        while 1 in next_tile.adjacent_tiles:
+            next_tile = next_tile.adjacent_tiles[1]
+            tiles_row.append(next_tile)
+        tiles_array.append(tiles_row)
+
+        if 2 in next_edge_tile.adjacent_tiles:
+            next_edge_tile = next_edge_tile.adjacent_tiles[2]
+        else:
+            break
+
+    return tiles_array
+
+
+def get_whole_picture(tiles_array):
+    whole_picture = []
+    for row in tiles_array:
+        row_data = [row_item.get_cut_tile_data() for row_item in row]
+        whole_picture.extend(list(''.join(line)) for line in zip(*row_data))
+    return whole_picture
+
+
+def get_nessie_count(image, pattern):
+    for rotate in range(4):
+        for flip in [True, False]:
+            new_image = transform_2d_array(image, rotate, flip)
+            pattern_count = get_pattern_count(new_image, pattern)
+            if pattern_count > 0:
+                return pattern_count
+
+
+def get_pattern_count(image, pattern):
+    pattern_count = 0
+    for y in range(0, len(image) - len(pattern) + 1):
+        for x in range(0, len(image[y]) - len(pattern[0]) + 1):
+            sub_image = []
+            for sub_y in range(len(pattern)):
+                sub_image.append(image[y + sub_y][x:x + len(pattern[sub_y])])
+            if is_pattern_present(sub_image, pattern):
+                pattern_count += 1
+    return pattern_count
+
+
+def is_pattern_present(image, pattern):
+    for y in range(len(pattern)):
+        for x in range(len(pattern[0])):
+            if pattern[y][x] != ' ' and pattern[y][x] != image[y][x]:
+                return False
+    return True
+
+
+def count_chars(image, char):
+    char_count = 0
+    for row in image:
+        for c in row:
+            if c == char:
+                char_count += 1
+    return char_count
+
+
 class Tile:
 
     def __init__(self, tile_id, tile_data):
@@ -51,50 +189,102 @@ class Tile:
         self.tile_cells = [[]]
         self.edge_length = 0
         self.load_tile_data()
-        self.edges = []
-        self.flipped_edges = []
+        self.edges = {}
+        self.flipped_edges = {}
         self.load_edges()
         self.neighbours = set()
         self.flipped_neighbours = set()
-        self.adjacent_tiles = []
+        self.adjacent_tiles = {}
+        self.is_flipped = 'none'
+        self.is_rotated = False
 
     def load_tile_data(self):
+        """
+        Initialize tile with data and set edge length
+        """
         self.tile_cells = []
         for tile_line in self.tile_data:
             self.tile_cells.append([c for c in tile_line])
         self.edge_length = len(self.tile_data[0])
 
     def load_edges(self):
-        self.edges = []
-        self.flipped_edges = []
+        """
+        Initialize list of edges and flipped edges. Edges are indexed 0-4 starting from top and going clockwise
+        """
+        self.edges = {}
+        self.flipped_edges = {}
+        flipped_cells = transform_2d_array(self.tile_cells, 0, True)
         # Top edge
-        self.edges.append(''.join(self.tile_cells[0]))
+        self.edges[0] = ''.join(self.tile_cells[0])
+        self.flipped_edges[0] = ''.join(flipped_cells[0])
         # Right edge
-        self.edges.append(''.join([line[self.edge_length-1] for line in self.tile_cells]))
+        self.edges[1] = ''.join([line[self.edge_length-1] for line in self.tile_cells])
+        self.flipped_edges[1] = ''.join([line[self.edge_length-1] for line in flipped_cells])
         # Bottom edge
-        self.edges.append(''.join(self.tile_cells[self.edge_length - 1])[::-1])
+        self.edges[2] = ''.join(self.tile_cells[self.edge_length - 1])[::-1]
+        self.flipped_edges[2] = ''.join(flipped_cells[self.edge_length - 1])[::-1]
         # Left edge
-        self.edges.append(''.join([line[0] for line in self.tile_cells][::-1]))
-        for edge in self.edges:
-            self.flipped_edges.append(edge[::-1])
+        self.edges[3] = ''.join([line[0] for line in self.tile_cells][::-1])
+        self.flipped_edges[3] = ''.join([line[0] for line in flipped_cells][::-1])
 
     def set_adjacent_tiles(self, other_tiles):
-        self.adjacent_tiles = []
+        self.adjacent_tiles = {}
+        self.neighbours = set()
+        self.flipped_neighbours = set()
         for other_tile in other_tiles:
-            if self.is_adjacent(other_tile):
-                self.adjacent_tiles.append(other_tile)
+            is_adjacent, edge_id = self.is_adjacent(other_tile)
+            if is_adjacent:
+                self.adjacent_tiles[edge_id] = other_tile
 
     def is_adjacent(self, other):
         if self == other:
-            return False
-        for edge in self.edges:
-            if edge in other.edges:
-                self.neighbours.add(other)
-                return True
-            elif edge in other.flipped_edges:
+            return False, 0
+        for edge_id, edge in self.edges.items():
+            if edge in other.edges.values():
                 self.flipped_neighbours.add(other)
-                return True
-        return False
+                return True, edge_id
+            elif edge in other.flipped_edges.values():
+                self.neighbours.add(other)
+                return True, edge_id
+        return False, 0
+
+    def set_neighbors_flipped(self, is_current_flipped):
+        if self.is_flipped != 'none':
+            return
+        self.is_flipped = is_current_flipped
+        for neighbour in self.neighbours:
+            neighbour.set_neighbors_flipped(self.is_flipped)
+        for neighbour in self.flipped_neighbours:
+            if self.is_flipped == 'normal':
+                neighbour.set_neighbors_flipped('flipped')
+            elif self.is_flipped == 'flipped':
+                neighbour.set_neighbors_flipped('normal')
+
+    def rotate_neighbours(self, all_tiles):
+        for direction, neighbour in self.adjacent_tiles.items():
+            if not neighbour.is_rotated:
+                neighbour.rotate_to_match(self, direction, all_tiles)
+
+    def rotate_to_match(self, other_tile, other_tile_direction, all_tiles):
+        """
+        Will rotate current tile to align directions of the edges relative to a neighbour tile
+        """
+        self.is_rotated = True
+        for my_direction, tile in self.adjacent_tiles.items():
+            if tile.tile_id == other_tile.tile_id:
+                direction_required = (other_tile_direction - 2) % 4
+                rotate_angle = rotation_matrix[direction_required][my_direction]
+                if rotate_angle != 0:
+                    self.tile_cells = transform_2d_array(self.tile_cells, rotate_angle, False)
+                    self.load_edges()
+                    self.set_adjacent_tiles(all_tiles.values())
+        self.rotate_neighbours(all_tiles)
+
+    def get_cut_tile_data(self):
+        cut_tile_data = []
+        for i in range(1, len(self.tile_cells)-1):
+            cut_tile_data.append(''.join(self.tile_cells[i][1:-1]))
+        return cut_tile_data
 
     def __eq__(self, other):
         return self.tile_id == other.tile_id
--- day20/main_test.py
+++ day20/main_test.py
@@ -29,9 +29,13 @@ class MainTestCase(unittest.TestCase):
 
     def test_is_adjacent_detects_adjacent_pair(self):
         tiles = main.parse_tiles(self.tiles_txt)
-        self.assertTrue(tiles[1951].is_adjacent(tiles[2729]))
-        self.assertTrue(tiles[2473].is_adjacent(tiles[1427]))
-        self.assertFalse(tiles[2971].is_adjacent(tiles[1427]))
+
+        is_adjacent, _ = tiles[1951].is_adjacent(tiles[2729])
+        self.assertTrue(is_adjacent)
+        is_adjacent, _ = tiles[2473].is_adjacent(tiles[1427])
+        self.assertTrue(is_adjacent)
+        is_adjacent, _ = tiles[2971].is_adjacent(tiles[1427])
+        self.assertFalse(is_adjacent)
 
     def test_get_adjacent_tiles_returns_correct_count(self):
         tiles = main.parse_tiles(self.tiles_txt)
@@ -45,3 +49,85 @@ class MainTestCase(unittest.TestCase):
         self.assertIn(2971, corner_tile_ids)
         self.assertIn(1171, corner_tile_ids)
         self.assertEqual(len(corner_tile_ids), 4)
+
+    def test_set_neighbours_flipped_sets_correct_flag(self):
+        tiles = main.parse_tiles(self.tiles_txt)
+        main.set_adjacent_tiles(tiles)
+        corner_tile = tiles[1951]
+        corner_tile.set_neighbors_flipped('normal')
+        self.assertEqual(corner_tile.is_flipped, 'normal')
+        self.assertEqual(tiles[1171].is_flipped, 'normal')
+        self.assertEqual(tiles[3079].is_flipped, 'flipped')
+
+    def test_normalize_tiles_return_correct_orientation(self):
+        tiles = main.parse_tiles(self.tiles_txt)
+        main.set_adjacent_tiles(tiles)
+        corner_tile = tiles[1951]
+        main.normalize_tiles(corner_tile.tile_id, tiles)
+        self.assertEqual(tiles[1171].adjacent_tiles[3].tile_id, 2473)
+
+    def test_compile_tile_array_returns_correct_array(self):
+        tiles = main.parse_tiles(self.tiles_txt)
+        main.set_adjacent_tiles(tiles)
+        main.normalize_tiles(1951, tiles)
+        actual = main.compile_tile_array(1951, tiles)
+        self.assertEqual(len(actual), 3)
+        self.assertEqual(len(actual[0]), 3)
+        self.assertEqual(actual[0][0].tile_id, 1951)
+        self.assertEqual(actual[0][1].tile_id, 2729)
+        self.assertEqual(actual[0][2].tile_id, 2971)
+        self.assertEqual(actual[1][0].tile_id, 2311)
+        self.assertEqual(actual[1][1].tile_id, 1427)
+        self.assertEqual(actual[1][2].tile_id, 1489)
+        self.assertEqual(actual[2][0].tile_id, 3079)
+        self.assertEqual(actual[2][1].tile_id, 2473)
+        self.assertEqual(actual[2][2].tile_id, 1171)
+
+    def test_flipped_edge_same_as_transformed(self):
+        tile = main.parse_tiles(self.tiles_txt)[1951]
+        flipped_data = main.transform_2d_array(tile.tile_cells, 0, True)
+        self.assertEqual(tile.flipped_edges[0], ''.join(flipped_data[0]))
+
+    def test_get_whole_picture_returns_correct_data(self):
+        tiles = main.parse_tiles(self.tiles_txt)
+        main.set_adjacent_tiles(tiles)
+        main.normalize_tiles(1951, tiles)
+        tile_array = main.compile_tile_array(1951, tiles)
+
+        actual = main.get_whole_picture(tile_array)
+        actual = main.transform_2d_array(actual, 3, True)
+
+        self.assertEqual(''.join(actual[0]), '.#.#..#.##...#.##..#####')
+        self.assertEqual(''.join(actual[len(actual)-1]), '...###...##...#...#..###')
+
+    def test_is_pattern_present_finds_valid_pattern(self):
+        image = ['.##.#',
+                 '#.###']
+        pattern = [' #  #',
+                   '# # #']
+        self.assertTrue(main.is_pattern_present(image, pattern))
+
+    def test_is_pattern_present_finds_invalid_pattern(self):
+        image = ['.##..',
+                 '#.###']
+        pattern = [' #  #',
+                   '# # #']
+        self.assertFalse(main.is_pattern_present(image, pattern))
+
+    def test_get_pattern_count_returns_correct_count(self):
+        image = ['.##..',
+                 '#.##.',
+                 '#.#.#']
+        pattern = [' # ',
+                   '# #']
+        self.assertEqual(main.get_pattern_count(image, pattern), 2)
+
+    def test_get_nessie_count_returns_correct_count(self):
+        tiles = main.parse_tiles(self.tiles_txt)
+        main.set_adjacent_tiles(tiles)
+        main.normalize_tiles(1951, tiles)
+        tile_array = main.compile_tile_array(1951, tiles)
+
+        image = main.get_whole_picture(tile_array)
+        actual = main.get_nessie_count(image, main.sea_monster)
+        self.assertEqual(actual, 2)
